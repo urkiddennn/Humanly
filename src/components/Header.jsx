b2814b06bf54f31686f2e4468d2dcb56b2fe3834
urkiddennn <154726035+urkiddennn@users.noreply.github.com>
2025-01-10 09:09:53 +0800

    Updated the promt output

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,7 +7,7 @@ function Header() {
     <div className="w-3/4 h-28 pr-4 pl-4 flex justify-between text-white items-center rounded-lg bg-green-600">
       <div className="flex items-center justify-center gap-3 text-3xl font-bold">
         <SiRobotframework className="text-5xl text-white" />
-        <h1>Humaly.AI</h1>
+        <h1>Humanly.AI</h1>
       </div>
       <div className="flex items-center justify-center gap-3 text-3xl">
         <a href="https://www.facebook.com/Urkidden">
--- src/models/Model.jsx
+++ src/models/Model.jsx
@@ -6,7 +6,7 @@ const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
 export const generateContent = async (prompt) => {
   const result = await model.generateContent(
     prompt +
-      "Make it humanly so that it doesn't look like it is AI generated. Make sure it is not long"
+      "Make it humanly so that it doesn't look like it is AI generated. Make sure not to change the context."
   );
   console.log(result.response.text());
   return result.response.text; // return the response
