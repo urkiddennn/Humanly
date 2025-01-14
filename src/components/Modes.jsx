515fbdae5d4b842a220b43531f99507d8653932a
urkiddennn <154726035+urkiddennn@users.noreply.github.com>
2025-01-14 11:45:15 +0800

    updated the modes

--- src/components/Modes.jsx
+++ src/components/Modes.jsx
@@ -7,15 +7,34 @@ export function Modes() {
       value: "Simple",
     },
     {
-      label: "advance",
-      value: "advance",
+      label: "Advanced",
+      value: "Advanced",
     },
   ];
   const [value, setValue] = useState("");
 
+  const handleClick = (option) => {
+    setValue(option.value);
+  };
+
   return (
     <div className="pl-4 pr-4 w-3/4 h-28 border border-gray-300 rounded-lg flex justify-start gap-10 items-center">
       <h1 className="text-2xl text-gray-500 font-semibold">Options:</h1>
+      <div className="flex  h-full">
+        {options.map((option) => (
+          <button
+            key={option.value}
+            className={`border-r border-l border-t-0 border-b-0 font-semibold text-2xl  border-gray-300 px-4 py-2  h-full  ${
+              value === option.value
+                ? "bg-green-600 text-white"
+                : "text-gray-500"
+            }`}
+            onClick={() => handleClick(option)}
+          >
+            {option.label}
+          </button>
+        ))}
+      </div>
     </div>
   );
 }
