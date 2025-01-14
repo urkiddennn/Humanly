7e0f20f1dc882e94c814d0df22059556572a1b02
urkiddennn <154726035+urkiddennn@users.noreply.github.com>
2025-01-14 19:09:49 +0800

    update again

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ import { FaFacebook, FaGithub } from "react-icons/fa";
 
 function Header() {
   return (
-    <div className="md:w-3/4 w-full h-28 pr-4 pl-4 flex justify-between text-white items-center rounded-lg bg-green-600">
+    <div className="md:w-3/4 w-full h-20 md:h-28 pr-4 pl-4 flex justify-between text-white items-center rounded-lg bg-green-600">
       <div className="flex items-center justify-center gap-3 text-xl md:text-3xl font-bold">
         <SiRobotframework className="text-3xl md:text-5xl text-white" />
         <h1>Humanly.AI</h1>
