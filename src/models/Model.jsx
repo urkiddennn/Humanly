64f99197c7229ed638364283ce4a8de05647e750
urkiddennn <154726035+urkiddennn@users.noreply.github.com>
2025-01-14 16:21:12 +0800

    updated the prompt

--- src/models/Model.jsx
+++ src/models/Model.jsx
@@ -7,7 +7,7 @@ export const generateContent = async (prompt, mode) => {
   const result = await model.generateContent(
     prompt +
       `(Mode: ${mode})` +
-      "Make it humanly so that it doesn't look like it is AI generated. Make sure not to change the context."
+      "Make it humanly so that it doesn't look like it is AI generated. Make sure not to change the context or  format."
   );
   console.log(result.response.text());
   return result.response.text;
